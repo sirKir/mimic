0cc1de70dc97710730b5de0b1c49de20d8bd7c51
sirKir <58689720+sirKir@users.noreply.github.com>
2021-06-05 14:43:44 +0300

    Add files via upload

--- core.js
+++ core.js
@@ -1,11 +1,32 @@
-let loginEntry = document.querySelector('#login-entry');
-let qrEntry = document.querySelector('#qr-entry');
-let loginGroup = document.querySelector(".login__group");
-let loginGroupForm = document.querySelector(".login__group_form");
+const loginEntry = document.querySelector('#login-entry');
+const qrEntry = document.querySelector('#qr-entry');
+const loginGroup = document.querySelector(".login__group");
+const loginGroupForm = document.querySelector(".login__group_form");
+const loginGroupFormQr = document.querySelector(".login__group_form_qr");
+const backBtn = document.querySelector('#backBtn');
+const qrBackBtn = document.querySelector('#qrBackBtn');
 
 
 loginEntry.addEventListener('click', (e) =>  {
   e.preventDefault;
   loginGroup.classList.add('hided');
   loginGroupForm.classList.remove('hided')
+})
+backBtn.addEventListener('click', (e) =>  {
+  e.preventDefault;
+  loginGroupForm.classList.add('hided');
+  loginGroup.classList.remove('hided');
+})
+
+
+
+qrEntry.addEventListener('click', (e) => {
+  e.preventDefault;
+  loginGroup.classList.add('hided');
+  loginGroupFormQr.classList.remove('hided')
+})
+qrBackBtn.addEventListener('click', (e) =>  {
+  e.preventDefault;
+  loginGroupFormQr.classList.add('hided');
+  loginGroup.classList.remove('hided');
 })
